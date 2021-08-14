8b2e68a2295facd6d35a7a6a46958e4544350319
Saurav Kumar <https://github.com/sauravkumar-2002/Health-Companion>
2021-08-15 01:32:16 +0530

    new commit with create grup

--- app/src/main/java/com/example/eduapp/teachers/MainActivity.java
+++ app/src/main/java/com/example/eduapp/teachers/MainActivity.java
@@ -1,20 +1,29 @@
-package com.example.eduapp.student;
+package com.example.eduapp.teachers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.eduapp.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class studentHome extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_home);
+        setContentView(R.layout.activity_main);
+        // commit
+        int a;
+        String saurav;
         DatabaseReference reference= FirebaseDatabase.getInstance().getReference("ghghg");
         reference.push().setValue("dfg");
+    }
+
+    public void cghek(View view) {
+        startActivity(new Intent(this,teachers_dashboard.class));
     }
 }
