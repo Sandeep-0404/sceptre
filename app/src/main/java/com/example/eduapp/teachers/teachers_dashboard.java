442eee0548483bdc9bc43a63edebb5bbfbc6d153
Sandeep-0404 <sandeepgh2002@gmail.com>
2021-08-15 19:18:45 +0530

    add pdf

--- app/src/main/java/com/example/eduapp/teachers/teachers_dashboard.java
+++ app/src/main/java/com/example/eduapp/teachers/teachers_dashboard.java
@@ -1,40 +1,44 @@
 package com.example.eduapp.teachers;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 
-import com.example.eduapp.R;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.eduapp.databinding.ActivityTeachersDashboardBinding;
 
 public class teachers_dashboard extends AppCompatActivity {
-ActivityTeachersDashboardBinding binding;
-String username="jkjkjk";
+    ActivityTeachersDashboardBinding binding;
+    String username = "jkjkjk";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityTeachersDashboardBinding.inflate(getLayoutInflater());
+        binding = ActivityTeachersDashboardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
 
-binding.create.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        Bundle bundle=new Bundle();
-        bundle.putString("username",username);
+        binding.create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("username", username);
 
-        frag_for_create frag_for_create=new frag_for_create();
-        frag_for_create.setArguments(bundle);
-        frag_for_create.show(getSupportFragmentManager(),frag_for_create.getTag());
+                frag_for_create frag_for_create = new frag_for_create();
+                frag_for_create.setArguments(bundle);
+                frag_for_create.show(getSupportFragmentManager(), frag_for_create.getTag());
 
-    }
-});
-
-
+            }
+        });
 
 
+        binding.assignmentCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),uploadAssignment.class));
+            }
+        });
 
     }
 }
